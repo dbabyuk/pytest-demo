70ce6ed22ab92e30202f93f58ff744582562d5fd
Dmytro Babyuk <d_babyuk@yahoo.com>
2022-07-18 19:25:44 +0300

    stages 2-3 completed:

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,12 +7,55 @@ class MainPage:
         self.driver = driver
 
     _title = (By.CSS_SELECTOR, 'span[class="title"]')
+    _cart = (By.CSS_SELECTOR, 'a[class="shopping_cart_link"]')
+    _item_name = (By.CSS_SELECTOR, 'div[class="inventory_item_name"]')
+    _add_to_cart = (By.CSS_SELECTOR, 'button[name*="add-to-cart"]')
+    _image = (By.CSS_SELECTOR, 'img[class="inventory_item_img"]')
+
     _menu = (By.CSS_SELECTOR, 'button[id*="menu"]')
     _logout = (By.CSS_SELECTOR, 'a[id*="logout"]')
 
     def get_title(self):
         webelement = self.driver.find_element(*self._title)
         return webelement.text
+
+    def cart_icon_content(self):
+        webelement = self.driver.find_element(*self._cart)
+        txt = webelement.text
+        return txt
+
+    def inventory_items(self):
+        webelements = self.driver.find_elements(*self._item_name)
+        res = []
+        for elem in webelements:
+            res.append(elem.text)
+        return res
+
+    def image_is_present(self, index_=0):
+        try:
+            webelements = self.driver.find_elements(*self._image)[index_]
+            return True
+        except:
+            return False
+
+    def font_color(self, index_=0, object_element='title'):
+        if object_element == 'title':
+            webelements = self.driver.find_elements(*self._item_name)
+        else:
+            webelements = self.driver.find_elements(*self._add_to_cart)
+        rgb_color = webelements[index_].value_of_css_property('color')
+        if rgb_color == 'rgba(226, 35, 26, 1)':
+            res = 'red'
+        else:
+            res = None
+        return res
+
+    def item_title_format(self, index_=0):
+        webelements = self.driver.find_elements(*self._item_name)
+        res = webelements[index_].text
+        return res
+
+
 
     def _click_menu_btn(self):
         self.driver.find_element(*self._menu).click()
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,4 +1,46 @@
+import pytest
 
 
 def test_user_is_logged_in(main_page):
     assert main_page.get_title() == 'PRODUCTS'
+
+
+@pytest.mark.unit
+def test_cart_is_empty(main_page):
+    assert main_page.cart_icon_content() == ''
+
+
+@pytest.fixture(scope='module')
+def get_inventory_items(main_page):
+    return main_page.inventory_items()
+
+
+def test_inventory_number(get_inventory_items):
+    assert len(get_inventory_items) == 6
+
+
+def test_inventory_ordered_alphabetically(get_inventory_items):
+    assert get_inventory_items == sorted(get_inventory_items)
+
+
+@pytest.mark.parametrize('index_', range(6))
+def test_image_is_present(main_page, index_):
+    assert main_page.image_is_present(index_)
+
+
+@pytest.mark.parametrize('index_', range(6))
+def test_red_font_for_item_title(main_page, index_):
+    assert main_page.font_color(index_) == 'red'
+
+
+@pytest.mark.parametrize('index_', range(6))
+def test_item_title_capitalized(main_page, index_):
+    res = main_page.item_title_format(index_).split()
+    assert res
+
+
+@pytest.mark.parametrize('index_', range(6))
+def test_add_to_cart_red_font(main_page, index_):
+    res = main_page.font_color(index_, 'add_to_cart')
+    assert res == 'red'
+
