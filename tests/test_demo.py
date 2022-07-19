d1dfd7adabca6a4ded49a901b7273f9449b75835
Dmytro Babyuk <d_babyuk@yahoo.com>
2022-07-19 14:48:32 +0300

    test ids added

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -23,23 +23,26 @@ def test_inventory_ordered_alphabetically(get_inventory_items):
     assert get_inventory_items == sorted(get_inventory_items)
 
 
-@pytest.mark.parametrize('index_', range(6))
+_ids = {item_index: f'Product_{item_index + 1}' for item_index in range(6)}
+
+
+@pytest.mark.parametrize('index_', _ids.keys(), ids=_ids.values())
 def test_image_is_present(main_page, index_):
     assert main_page.image_is_present(index_)
 
 
-@pytest.mark.parametrize('index_', range(6))
+@pytest.mark.parametrize('index_', _ids.keys(), ids=_ids.values())
 def test_red_font_for_item_title(main_page, index_):
     assert main_page.font_color(index_) == 'red'
 
 
-@pytest.mark.parametrize('index_', range(6))
+@pytest.mark.parametrize('index_', _ids.keys(), ids=_ids.values())
 def test_item_title_capitalized(main_page, index_):
     res = main_page.item_title_format(index_).split()
     assert res
 
 
-@pytest.mark.parametrize('index_', range(6))
+@pytest.mark.parametrize('index_', _ids.keys(), ids=_ids.values())
 def test_add_to_cart_red_font(main_page, index_):
     res = main_page.font_color(index_, 'add_to_cart')
     assert res == 'red'
